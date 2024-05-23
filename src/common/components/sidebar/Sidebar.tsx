037cf757be698230754d3bcacd650ffa53dbfe2c
墨綠BG <s5460703@gmail.com>
2024-05-24 00:51:23 +0800

    🔨 refactor：封装侧边栏

---
+++ src/common/components/sidebar/Sidebar.tsx
@@ -0,0 +1,21 @@
+import useStore from '@/common/hooks/useStore';
+
+import Navigation from './Navigation';
+import TopArea from './TopArea';
+
+const Sidebar: React.FC = () => {
+  const { activeMenu } = useStore();
+
+  return (
+    <div className="ml-3 h-screen overflow-auto pb-10 md:overflow-hidden md:hover:overflow-auto">
+      {activeMenu && (
+        <>
+          <TopArea />
+          <Navigation />
+        </>
+      )}
+    </div>
+  );
+};
+
+export default Sidebar;
---
+++ src/common/components/sidebar/TopArea.tsx
@@ -0,0 +1,27 @@
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+import { MdOutlineCancel } from 'react-icons/md';
+
+import useStore from '@/common/hooks/useStore';
+
+import Logo from './Logo';
+
+const TopArea: React.FC = () => {
+  const { currentColor, activeMenu, setActiveMenu } = useStore();
+
+  return (
+    <div className="flex items-center justify-between">
+      <Logo />
+      <TooltipComponent content="Menu" position="BottomCenter">
+        <button
+          type="button"
+          onClick={() => setActiveMenu(!activeMenu)}
+          style={{ color: currentColor }}
+          className="mt-4 block rounded-full p-3 text-xl hover:bg-light-gray md:hidden"
+        >
+          <MdOutlineCancel />
+        </button>
+      </TooltipComponent>
+    </div>
+  );
+};
+export default TopArea;
