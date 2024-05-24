bed32dddce32848f19b460cd2241fe8b10dadcfd
墨綠BG <s5460703@gmail.com>
2024-05-24 23:33:02 +0800

    ✨ feat：新增 Calendar 组件

---
+++ src/app/(apps)/calendar/page.tsx
@@ -0,0 +1,13 @@
+import { NextPage } from 'next';
+
+import Calendar from '@/modules/Calendar';
+
+const Page: NextPage = () => {
+  return (
+    <>
+      <Calendar />
+    </>
+  );
+};
+
+export default Page;
