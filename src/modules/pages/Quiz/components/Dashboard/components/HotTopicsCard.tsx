00bc1647d3fa12ab83ea8c2cc55537f899299036
墨綠BG <s5460703@gmail.com>
2024-05-27 04:05:59 +0800

    ✨ feat：新增文字云组件

--- src/modules/pages/Quiz/components/Dashboard/components/HotTopicsCard.tsx
+++ src/modules/pages/Quiz/components/Dashboard/components/HotTopicsCard.tsx
@@ -1,14 +1,30 @@
-import React from "react";
+import React from 'react';
 
+import WordCloud from '@/common/components/elements/Charts/WordCloud';
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/common/components/ui/card";
-// import WordCloud from "../WordCloud";
-import { prisma } from "@/common/utils/db";
+} from '@/common/components/ui/card';
+// import { prisma } from "@/common/utils/db";
+
+const mockData = [
+  { text: 'JavaScript', value: 100 },
+  { text: 'TypeScript', value: 80 },
+  { text: 'React', value: 60 },
+  { text: 'Node.js', value: 50 },
+  { text: 'Angular', value: 40 },
+  { text: 'Vue.js', value: 30 },
+  { text: 'Svelte', value: 20 },
+  { text: 'HTML', value: 10 },
+  { text: 'CSS', value: 8 },
+  { text: 'Sass', value: 6 },
+  { text: 'Less', value: 4 },
+  { text: 'PostCSS', value: 2 },
+  { text: 'Webpack', value: 1 },
+];
 
 const HotTopicsCard: React.FC = React.memo(async () => {
   // const topics = await prisma.topic_count.findMany({});
@@ -23,13 +39,11 @@ const HotTopicsCard: React.FC = React.memo(async () => {
     <Card className="col-span-4">
       <CardHeader>
         <CardTitle className="text-2xl font-bold">热门主题</CardTitle>
-        <CardDescription>
-          点选一个主题即可开始测验。
-        </CardDescription>
+        <CardDescription>点选一个主题即可开始测验。</CardDescription>
       </CardHeader>
       <CardContent className="pl-2">
-        WordCloud
         {/* <WordCloud formattedTopics={formattedTopics} /> */}
+        <WordCloud formattedTopics={mockData} />
       </CardContent>
     </Card>
   );
